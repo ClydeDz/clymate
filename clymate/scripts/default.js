bcfb9effb2550b13994dd862aa3b83d030be26b1
Clyde D'Souza <clyd94@gmail.com>
2016-01-04 18:35:57 +1300

    cards for general temperature data

--- clymate/scripts/default.js
+++ clymate/scripts/default.js
@@ -1,4 +1,4 @@
-﻿// page load
+﻿// page load+
 // look for cookies
 // // if cookies then set default variables from there 
 // // else set default variables to preset values
@@ -12,14 +12,98 @@
 var default_city = "New York";
 var default_lat = 40.71;
 var default_long = -74.01;
+var default_unit = "imperial";
 
 function loadClymate() {
     // load settings
-    getCitiesList()
+    getCitiesList();
+    initializeHumidityGauge();
     // look for cookies
     checkCookie("clymate-city", "clymate-latitude", "clymate-longitude");
+    // load temper
+    getTemperature(default_city,default_unit);
 }
 
+function getTemperature(input,unit) {
+    weatherModule.getWeather(setTemperature);
+}
+function setTemperature(data) {
+    var temperatureMainText = "<div class='city-name'>"+data["name"]+"</div>";
+    temperatureMainText += "<div class='city-country'>" + data["sys"].country + "</div><div class='temperature-value'>" + toCelsius(data["main"].temp) + "<span class='temperature-unit'>&deg;C</span></div>";
+    temperatureMainText += "<div class='weather-description'>" + (data.weather[0].description).toString().charAt(0).toUpperCase().concat(data.weather[0].description.toString().substr(1, data.weather[0].description.toString().length - 1)) + "</div>";
+    document.getElementById('temperatureMain').innerHTML = "" + temperatureMainText;
+    document.getElementById("temperatureMain").style.background = "url('../images/" + data.weather[0].main.toString().toLowerCase() + "Timeline.jpe')  no-repeat scroll center center";
+    var minmaxText = "<div>Minimum: <h2>" + data["main"].temp_min + "</h2></div><div>Maximum: <h2>" + data["main"].temp_max + "</h2></div>";
+    document.getElementById('minMax').innerHTML = "" + minmaxText;
+    var humidityText = "<div>Humidity</div>";
+    //<div><h2>" + data["main"].humidity + "%</h2></div>";
+    //document.getElementById('humidity').innerHTML = "" + humidityText;
+    loadHumidityGauge(data["main"].humidity);
+
+    var windText = "<div>Wind</div><div>" + data["wind"].speed + " mph</div>";
+    document.getElementById('wind').innerHTML = "" + windText;
+    document.getElementById('wind').innerHTML = "" + windText;
+    document.getElementById('customGraphics').innerHTML = "<img src='images/" + data.weather[0].main.toString().toLowerCase() + ".png' class='img-responsive' height='150' width='150'/>";
+}
+function loadHumidityGauge(input) {
+    GaugeChart.load({
+        columns: [['Humidity', input]]
+    },1500);
+}
+function initializeHumidityGauge() {
+    GaugeChart = c3.generate({
+        bindto: '#humidityGauge',
+        data: {
+            columns: [
+                ['Humidity', Math.floor(Math.random() * 99) + 1]
+            ],
+            keys: {
+                value: ['Humidity']
+            },
+            type: 'gauge',
+            onclick: function (d, i) { /*console.log("onclick", d, i);*/ },
+            onmouseover: function (d, i) { /*console.log("onmouseover", d, i);*/ },
+            onmouseout: function (d, i) {/* console.log("onmouseout", d, i); */ }
+        },
+        gauge: {
+            label: {
+                format: function (value, ratio) {
+                    return value + "%";
+                },
+                show: false // to turn off the min/max labels.
+            },
+            min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+            max: 100, // 100 is default
+            units: ' %',
+            width: 49 // for adjusting arc thickness
+        },
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                //            unit: 'value', // percentage is default
+                //            max: 200, // 100 is default
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 180
+        }
+    });
+}
+var customGraphicsJson = [
+    { "name": "Thunderstorm", "image": "thunderstorm" },
+    { "name": "Drizzle", "image": "" },
+    { "name": "Rain", "image": "" },
+    { "name": "Snow", "image": "" },
+    { "name": "Atmosphere", "image": "" },
+    { "name": "Clear", "image": "" },
+    { "name": "Clouds", "image": "" },
+    { "name": "Extreme", "image": "" },
+    { "name": "Additional", "image": "" }
+];
+function toCelsius(faren) {
+    return ((faren - 32) * (5 / 9)).toFixed(2);
+}
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -53,3 +137,22 @@ function checkCookie(input,latitude,longitude) {
         setCookie(longitude, default_long, 30);
     }
 }
+
+/* API */
+var weatherModule = (function () {
+    return {
+        getWeather: function (callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + default_city + "&units="+default_unit+"&appid=2de143494c0b295cca9337e1e96b00e0",
+                success: function (data) {
+                    if (data["cod"] == 200)
+                        callback(data);
+                    else
+                        errorText();
+                }
+            });
+        }
+    };
+}());
